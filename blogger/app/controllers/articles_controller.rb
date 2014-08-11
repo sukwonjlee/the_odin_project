4b1fadba82e594e5d0d7c0ac5a15dceef2de592b
Sukwon Lee <sukwonjlee@gmail.com>
2014-08-11 15:37:02 -0400

    11. Form-based Workflow

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+	# app/helpers/articles_helper.rb for article_params
+	include ArticlesHelper 
 
 	def index
 		@articles = Article.all
@@ -6,6 +8,38 @@ class ArticlesController < ApplicationController
 
 	def show
 		@article = Article.find(params[:id])		
+	end
+
+	def new
+		@article = Article.new		
+	end
+
+	def create
+		# @article = Article.new
+		# @article.title = params[:article][:title]
+		# @article.body = params[:article][:body]
+
+		@article = Article.new(article_params)		
+		@article.save
+		flash.notice = "Article #{@article.title} Created!"
+		redirect_to article_path(@article.id)
+	end
+
+	def destroy
+		@article = Article.find(params[:id]).destroy
+		flash.notice = "Article #{@article.title} Deleted!"
+		redirect_to :action => :index
+	end
+
+	def edit
+		@article = Article.find(params[:id])		
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		@article.update(article_params)
+		flash.notice = "Article #{@article.title} Updated!"
+		redirect_to article_path(@article.id)		
 	end
 
 end
