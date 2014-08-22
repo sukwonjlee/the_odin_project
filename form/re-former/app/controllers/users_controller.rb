b7568efcfd3e3738e57b5181d521a84ac3e34cff
Sukwon Lee <sukwonjlee@gmail.com>
2014-08-22 15:49:55 -0400

    form_for

---
+++ form/re-former/app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+  def new
+  	@user = User.new
+  end
+
+  def create
+  	@user = User.new(user_params)
+  	if @user.save
+  		redirect_to action: "new"
+  	else
+  		render action: "new"
+  	end
+  end
+
+  private
+  def user_params
+  	params.require(:user).permit(:username, :email, :password)
+  end
+end
