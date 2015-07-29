270bed6eba3ff4de45a63709202dddcced717429
Sukwon Lee <sukwonjlee@gmail.com>
2015-07-29 16:54:21 -0400

    Working on Blogger - Form

--- 2015_summer/rails_blog/blogger/app/controllers/articles_controller.rb
+++ 2015_summer/rails_blog/blogger/app/controllers/articles_controller.rb
@@ -6,4 +6,57 @@ class ArticlesController < ApplicationController
 	def show
 		@article = Article.find(params[:id])
 	end
+
+	def new
+		@article = Article.new
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def create
+		# @article = Article.new
+		# @article.title = params[:article][:title]
+		# @article.body = params[:article][:body]
+		# @article.save
+
+		# We can clean it up!
+
+		# @article = Article.new(params[:article])
+		# @article.save
+
+		# We can clean it up more with private method!
+
+		@article = Article.new(article_params)
+		@article.save
+
+		flash.notice = "Article '#{@article.title}' Created!!"
+
+		redirect_to article_path(@article.id)
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		@article.update(article_params)
+
+		flash.notice = "Article '#{@article.title}' Updated!!"
+
+		redirect_to article_path(@article.id)
+	end
+
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+
+		flash.notice = "Article '#{@article.title}' Deleted!!"
+
+		redirect_to articles_path
+	end
+
+	private
+	def article_params
+		params.require(:article).permit(:title, :body)
+	end
+
 end
